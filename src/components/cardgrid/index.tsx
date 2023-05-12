ce268751f830e965aa5bb0f338368f22eff55006
cfsonwin <fashu_cheng@163.com>
2023-05-12 23:20:30 +0200

    add list view for card display; add filter function

--- src/components/cardgrid/index.tsx
+++ src/components/cardgrid/index.tsx
@@ -1,12 +1,16 @@
-import MyCard, {cardProps} from '../card'
-import { Col, Row } from 'antd';
-import { getApplications, createApplications } from '../../api/requests';
-import { redirect, useLoaderData } from 'react-router-dom';
-import React, {useState, useRef} from 'react'
-import { Form } from 'react-router-dom'
-import "./index.css"
-import { Button } from 'antd'
+import React, { SyntheticEvent, useState, useRef} from 'react'
+import { redirect, useLoaderData, Form, Link, useSubmit, useParams } from 'react-router-dom';
+import { Button, Segmented, Col, Row, SegmentedProps } from 'antd'
+import { AppstoreOutlined, BarsOutlined } from '@ant-design/icons';
+import { EditOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Space, Table, Tag, Dropdown, Select } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 
+import "./index.css"
+import MyCard, {cardProps} from '../card'
+import { getApplications, createApplications } from '../../api/requests';
+import {cardStEnum} from "../status"
 
 export async function loader({params}:any){
     const applications = await getApplications(params.user)
@@ -39,52 +43,181 @@ interface formData {
     avatar: string,
 }
 
+interface cardData {
+    "_id"?: string,
+    "card": cardProps,
+    "infos": string,
+    "user": string
+}
+
 interface applicationsInterface {
     status: number,
-    data: {
-        "_id"?: string,
-        "card": cardProps,
-        "infos": string,
-        "user": string
-    }[],
+    data: cardData[],
   }
   
-  interface LoaderData {
-    applications: applicationsInterface;
-  }
+interface LoaderData {
+applications: applicationsInterface;
+}
+
+enum tableTagColor {
+    "gray",
+    "blue",
+    "red",
+    "green"
+}
 
 export default function CardGrid() {
   const [showForm, setShowForm] = useState(false)
+  const [layout, setlayout] = useState("Kanban")
   const [refresh, setrefresh] = useState(false)
+  const { user } = useParams();
+  const submit = useSubmit()
   const { applications } = useLoaderData() as LoaderData
+  const [appList, setappList] = useState(applications)
+  const handleChange = (value: string)=>{
+    if(value==="All"){
+        setappList(applications)
+    }else{
+        setappList({
+            status:  applications["status"],
+            data: applications["data"].filter(v=>{
+                return cardStEnum[v.card.status] === value
+            })
+        })
+    }
+    
+  }
   const formWrapperRef = useRef<HTMLDivElement>(null)
+  const stateChangeHandler = (target:HTMLElement, _id:string)=>{
+    let formData = new FormData();
+    const selectedSt = target.innerHTML as keyof typeof cardStEnum
+    const newState = cardStEnum[selectedSt].toString()
+    formData.append("newState", newState)
+    formData.append("_id", _id as string)
+    submit(formData, {
+        action: `/applications/${user}/stateupdate/`,
+        method: "post",
+    })
+}
+  const switchChangeHandler = (value:SegmentedProps["defaultValue"])=>{
+    console.log(value);
+    if(value === "List"){
+        setlayout("List")
+    }else{
+        setlayout("Kanban")
+    }
+  }
+  const columns: ColumnsType<cardData> = [
+    {
+      title: 'Job titel',
+      dataIndex: 'card',
+      key: 'Job',
+      render: (card:cardProps) => <p>{card.title.substring(0,1).toUpperCase() + card.title.substring(1)}</p>,
+    },
+    {
+      title: 'Company',
+      dataIndex: 'card',
+      key: 'Company',
+      render: (card:cardProps) => <p>{card.company}</p>,
+    },
+    {
+        title: 'State',
+        key: 'tags',
+        render: (_, { card, _id }) => (
+        <>
+            <Tag color={tableTagColor[card.status]} key={_id}>{cardStEnum[card.status]}</Tag>
+        </>
+        ),
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: (_, {_id, card}) => {
+            const cardStArray = ["Preparing",
+                "Progressing",
+                "Rejected",
+                "Passed"].filter((v,i)=>i!=card.status)
+
+            const items: MenuProps['items'] = cardStArray.map((v,i)=>{
+                let key:string = (i + 1).toString()
+                return (
+                {
+                key: key,
+                label: (
+                <div onClick={ (e:SyntheticEvent) => stateChangeHandler(e.target as HTMLElement, _id as string) }>
+                {v}
+                </div>
+                ),
+                }
+                )
+            }) 
+            return (
+          <Space size="middle">
+            <Link to={`/applications/Lawi/details/${_id}`}>
+                    <EditOutlined style={{fontSize:20, color:"grey"}} />
+            </Link>
+            <Dropdown menu={{ items }} placement="bottom" arrow={{ pointAtCenter: true }}>
+                <SettingOutlined style={{fontSize:20, color:"grey"}}/>
+            </Dropdown>
+          </Space>
+        )},
+      },
+  ];
   return (
     <div>
         <div className='new-app-wrapper'>
-        <Button id='btn01' onClick={(e)=>{
-            if(!showForm){
-                (formWrapperRef.current as HTMLDivElement).style.width = "300px";
-                (e.target as HTMLElement).innerHTML = "<span>Cancle</span>"
-                
-            }else{
-                (formWrapperRef.current as HTMLDivElement).style.width = "0";
-                (e.target as HTMLElement).innerHTML = "<span>Create New Application</span>"
-            }
-            setShowForm(!showForm)
-        }}>Create New Application</Button>
-        <Button id='btn02' onClick={(e)=>{
-            if(!showForm){
-                (formWrapperRef.current as HTMLDivElement).style.height = "420px";
-                (formWrapperRef.current as HTMLDivElement).style.border = "1px solid black";
-                (e.target as HTMLElement).innerHTML = "<span>Cancle</span>"
-                
-            }else{
-                (formWrapperRef.current as HTMLDivElement).style.height = "0";
-                (formWrapperRef.current as HTMLDivElement).style.border = "none";
-                (e.target as HTMLElement).innerHTML = "<span>Create New Application</span>"
-            }
-            setShowForm(!showForm)
-        }}>Create New Application</Button>
+            <div className="function-wrapper">
+                <Segmented
+                    defaultValue={ 'Kanban' }
+                    options={[
+                    {
+                        value: 'Kanban',
+                        icon: <AppstoreOutlined />,
+                    },
+                    {
+                        value: 'List',
+                        icon: <BarsOutlined />,
+                    },
+                    ]}
+                    onChange={ switchChangeHandler }
+                />
+                <Select
+                    defaultValue="All"
+                    style={{ width: 120 }}
+                    onChange={handleChange}
+                    options= {["All", "Preparing",
+                    "Progressing",
+                    "Rejected",
+                    "Passed"].map((v)=>{return {"value": v, "label": v}})}
+                />
+                <Button id='btn01' onClick={(e)=>{
+                    if(!showForm){
+                        (formWrapperRef.current as HTMLDivElement).style.width = "300px";
+                        (e.target as HTMLElement).innerHTML = "<span>Cancle</span>"
+                        
+                    }else{
+                        (formWrapperRef.current as HTMLDivElement).style.width = "0";
+                        (e.target as HTMLElement).innerHTML = "<span>Create New Application</span>"
+                    }
+                    setShowForm(!showForm)
+                }}>Create New Application</Button>
+                <Button id='btn02' onClick={(e)=>{
+                    if(!showForm){
+                        (formWrapperRef.current as HTMLDivElement).style.height = "420px";
+                        (formWrapperRef.current as HTMLDivElement).style.border = "1px solid black";
+                        (e.target as HTMLElement).innerHTML = "<span>Cancle</span>"
+                        
+                    }else{
+                        (formWrapperRef.current as HTMLDivElement).style.height = "0";
+                        (formWrapperRef.current as HTMLDivElement).style.border = "none";
+                        (e.target as HTMLElement).innerHTML = "<span>Create New Application</span>"
+                    }
+                    setShowForm(!showForm)
+                }}>Create New Application</Button>
+            </div>
+        
+        
+        
         <div className='new-app-form' ref={ formWrapperRef }>
             <Form method='post' action='add'>
                 <p style={{"fontSize": 26}}>Add a new Application</p>
@@ -102,9 +235,9 @@ export default function CardGrid() {
             </Form>
         </div>
       </div>
-        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 },{ xs: 8, sm: 16, md: 24, lg: 32 }]}>
+      {(layout==="Kanban")?(<Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 },{ xs: 8, sm: 16, md: 24, lg: 32 }]}>
             {
-                applications.data.map((app)=>{
+                appList.data.map((app)=>{
                     return (
                         <Col className="gutter-row" xs={24} md={12} lg={8} xxl={6} key={app._id}>
                             <MyCard status={app.card.status} isrc={app.card.isrc} title={app.card.title} company={app.card.company} avatar={app.card.avatar} id={app._id}></MyCard>
@@ -112,7 +245,10 @@ export default function CardGrid() {
                     )
                 })
             }
-        </Row>
+        </Row>):(
+            <Table columns={columns} dataSource={appList.data} rowKey={(record)=>record._id?record._id:0}/>
+        )
+        }
     
     </div>
   )
--- src/components/status/index.tsx
+++ src/components/status/index.tsx
@@ -5,7 +5,7 @@ interface stProps {
     status: number
 }
 export enum cardStEnum {
-    "Preparing",
+    "Preparing"=0,
     "Progressing",
     "Rejected",
     "Passed", 
@@ -20,28 +20,4 @@ enum badgeStEnum {
 type badgeType = "success" | "processing" | "error" | "default" | "warning" | undefined
 export default function Status(props:stProps) {
     return  <div><Badge status={badgeStEnum[props.status] as badgeType} />&nbsp;&nbsp;{cardStEnum[props.status]}</div>
-
-    switch (props.status) {
-        case 0:
-            return (
-                <div><Badge status="default" />&nbsp;&nbsp;Preparing</div>
-              )
-        case 1:
-            return (
-                <div><Badge status="processing" />&nbsp;&nbsp;Progressing</div>
-              )
-        case 2:
-            return (
-                <div><Badge status="warning" />&nbsp;&nbsp;Rejected</div>
-                )
-        case 3:
-            return (
-                <div><Badge status="success" />&nbsp;&nbsp;Passed</div>
-                )
-        default:
-            return (
-                <div><Badge status="error" />&nbsp;&nbsp;Unknown</div>
-              )
-    }
-  
 }
